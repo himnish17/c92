e5f7a193e761ea75ba54f1c0b83310b20c8fbd77
himnish17 <81815516+himnish17@users.noreply.github.com>
2021-07-03 14:44:13 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function addUser() {
+    player_1=document.getElementById("player1_name_input").value;
+    player_2=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player_1_name",player_1);
+    localStorage.setItem("player_2_name",player_2);
+    window.location="game_page.html";
+}
